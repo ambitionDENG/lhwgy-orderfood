6d70e13020e07d7041f3e02892f06f36f067d4df
yz <yuanzhangyly@163.com>
2018-12-25 02:11:34 +0800

    修改feign 提交不过去

--- lhwgy-web/src/main/java/com/yczn/LhwgyWeb.java
+++ lhwgy-web/src/main/java/com/yczn/LhwgyWeb.java
@@ -3,6 +3,7 @@ package com.yczn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,6 +15,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class LhwgyWeb {
     public static void main(String[] args) {
         SpringApplication.run(LhwgyWeb.class, args);
--- lhwgy-web/src/main/java/com/yczn/feign/DcDishStyleFeign.java
+++ lhwgy-web/src/main/java/com/yczn/feign/DcDishStyleFeign.java
@@ -4,8 +4,6 @@ import com.yczn.feignclient.FeignClientFallback;
 import com.yczn.toolkit.ReturnJsonTemplate;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,9 +13,9 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @date: 2018-12-19
  * @time: 10:14
  */
-@FeignClient(name = "lhwgy-provider-up", fallback = FeignClientFallback.class)
+@FeignClient(name = "lhwgy-provider-up",fallback = FeignClientFallback.class)
 public interface DcDishStyleFeign {
 
-    @GetMapping("/todatFood")
+    @GetMapping("/shitang/todatFood")
     ReturnJsonTemplate todatFood();
 }
